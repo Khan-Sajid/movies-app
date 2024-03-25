dfc7d8e7bf37f41c7c6edff30c30037454b1b39e
Sajid Khan <sajidkhanjnv0@gmail.com>
2024-03-25 23:26:41 +0530

    updated code

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,6 +3,7 @@ import styles from "./footer.module.scss";
 import { Screen } from "@/utils/constants";
 
 const Footer = ({ showScreen }: { showScreen: (val: Screen) => void }) => {
+  
   return (
     <footer className={styles.footer}>
       <button onClick={() => showScreen(Screen.FAVORITE)}>Favorites</button>
